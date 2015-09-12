a3ec002be256200e8f5764d0b3839f6e11a57231
snori <snori@xord.org>
2015-09-12 10:53:39 +0900

    add const_symbol_accessor.

---
+++ xot/lib/xot/const_symbol_accessor.rb
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+
+module Xot
+
+
+  module ConstSymbolAccessor
+
+    def const_symbol_accessor (name, **symbol2const)
+      const_symbol_writer name, **symbol2const
+      const_symbol_reader name, **symbol2const
+    end
+
+    def const_symbol_writer (name, **symbol2const)
+      writer = "#{name}=".intern
+      setter = "bf_set_#{name}__".intern
+
+      alias_method setter, writer
+      private setter
+
+      define_method writer do |symbol|
+        const = symbol2const[symbol]
+        raise ArgumentError unless const
+        __send__ setter, const
+      end
+
+      name
+    end
+
+    def const_symbol_reader (name, **symbol2const)
+      reader = name.intern
+      getter = "bf_get_#{name}__".intern
+
+      alias_method getter, reader
+      private getter
+
+      const2symbol = symbol2const.reduce({}) {|h, (k, v)| h[v] = k; h}
+
+      define_method reader do
+        const  = __send__ getter
+        symbol = const2symbol[const]
+        raise "'#{const}' is unknown value." unless symbol
+        symbol
+      end
+
+      name
+    end
+
+  end# ConstSymbolAccessor
+
+
+end# Xot
+
+
+class Module
+
+  include Xot::ConstSymbolAccessor
+
+end# Module
---
+++ xot/test/test_const_symbol_accessor.rb
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+
+require_relative 'helper'
+
+
+class TestConstSymbolAccessor < Test::Unit::TestCase
+
+  Unknown = -1
+  Const0  = 0
+  Const1  = 1
+
+  class ConstAccessor
+    attr_accessor :const
+    alias set const=
+    alias get const
+    def initialize (&block)
+      @const = Const0
+      block.call self if block
+    end
+  end
+
+  class SymbolAccessor < ConstAccessor
+    const_symbol_accessor :const, {
+      const0: Const0,
+      const1: Const1
+    }
+  end
+
+  def const (&block)
+    ConstAccessor.new &block
+  end
+
+  def symbol (&block)
+    SymbolAccessor.new &block
+  end
+
+  def test_const_accessor ()
+    assert_equal Const0, const.get
+    assert_equal Const1, const{|o| o.const = Const1}.get
+    assert_nothing_raised {const.const = Unknown}
+  end
+
+  def test_symbol_writer ()
+    assert_equal Const0, symbol.get
+    assert_equal Const1, symbol{|o| o.const = :const1}.get
+    assert_raise(ArgumentError) {symbol.const = :unknown}
+  end
+
+  def test_symbol_reader ()
+    assert_equal :const0, symbol.const
+    assert_equal :const1, symbol{|o| o.set Const1}.const
+    assert_raise(RuntimeError) {symbol{|o| o.set Unknown}.const}
+  end
+
+end# TestBitFlagAccessor
