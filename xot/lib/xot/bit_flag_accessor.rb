745e1e1ad2157e61efc5b4e9661b355fe16fa52f
snori <snori@xord.org>
2015-09-12 11:01:25 +0900

    refactoring.

--- xot/lib/xot/bit_flag_accessor.rb
+++ xot/lib/xot/bit_flag_accessor.rb
@@ -5,61 +5,74 @@ require 'xot/bit_flag'
 require 'xot/block_util'
 
 
-class Module
+module Xot
 
-  def bit_flag_accessor (name, bit_flag = nil, &block)
-    bf = define_bit_flag name, bit_flag, block
-    define_bit_flag_writer name, bf
-    define_bit_flag_reader name, bf
-  end
 
-  def bit_flag_writer (name, bit_flag = nil, &block)
-    define_bit_flag_writer name, define_bit_flag(name, bit_flag, block)
-  end
+  module BitFlagAccessor
 
-  def bit_flag_reader (name, bit_flag = nil, &block)
-    define_bit_flag_reader name, define_bit_flag(name, bit_flag, block)
-  end
+    def bit_flag_accessor (name, bit_flag = nil, &block)
+      bf = define_bit_flag name, bit_flag, block
+      define_bit_flag_writer name, bf
+      define_bit_flag_reader name, bf
+    end
 
-  private
+    def bit_flag_writer (name, bit_flag = nil, &block)
+      define_bit_flag_writer name, define_bit_flag(name, bit_flag, block)
+    end
 
-    def define_bit_flag (name, bit_flag, block)
-      bit_flag ||= Xot::BitFlag.new
-      Xot::BlockUtil.instance_eval_or_block_call bit_flag, &block if block
+    def bit_flag_reader (name, bit_flag = nil, &block)
+      define_bit_flag_reader name, define_bit_flag(name, bit_flag, block)
+    end
 
-      define_singleton_method "#{name}_flag".intern do
+    private
+
+      def define_bit_flag (name, bit_flag, block)
+        bit_flag ||= Xot::BitFlag.new
+        Xot::BlockUtil.instance_eval_or_block_call bit_flag, &block if block
+
+        define_singleton_method "#{name}_flag".intern do
+          bit_flag
+        end
+
         bit_flag
       end
 
-      bit_flag
-    end
+      def define_bit_flag_writer (name, bit_flag)
+        writer = "#{name}=".intern
+        setter = "bf_set_#{name}__".intern
 
-    def define_bit_flag_writer (name, bit_flag)
-      writer = "#{name}=".intern
-      setter = "bf_set_#{name}__".intern
+        alias_method setter, writer
+        private setter
 
-      alias_method setter, writer
-      private setter
+        define_method writer do |*symbols|
+          __send__ setter, bit_flag.symbols2bits(symbols)
+        end
 
-      define_method writer do |*symbols|
-        __send__ setter, bit_flag.symbols2bits(symbols)
+        name
       end
 
-      name
-    end
+      def define_bit_flag_reader (name, bit_flag)
+        reader = name.intern
+        getter = "bf_get_#{name}__".intern
 
-    def define_bit_flag_reader (name, bit_flag)
-      reader = name.intern
-      getter = "bf_get_#{name}__".intern
+        alias_method getter, reader
+        private getter
 
-      alias_method getter, reader
-      private getter
+        define_method reader do
+          bit_flag.bits2symbols __send__(getter)
+        end
 
-      define_method reader do
-        bit_flag.bits2symbols __send__(getter)
+        name
       end
 
-      name
-    end
+  end# BitFlagAccessor
+
+
+end# Xot
+
+
+class Module
+
+  include Xot::BitFlagAccessor
 
 end# Module
--- xot/lib/xot/universal_accessor.rb
+++ xot/lib/xot/universal_accessor.rb
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
 
 
+module Xot
+
+
+  module UniversalAccessor
+
+    def universal_accessor (*names, **options)
+      names.each do |name|
+        define_universal_accessor name, options
+      end
+    end
+
+    private
+
+      def define_universal_accessor (name, options)
+        set = "ua_set_#{name}__"
+        get = "ua_get_#{name}__"
+        class_eval <<-END
+          alias_method :#{set}, options[:writer] || :#{name}=
+          alias_method :#{get}, options[:reader] || :#{name}
+          private :#{set}, :#{get}
+          def #{name} (*args)
+            #{set} *args unless args.empty?
+            #{get}
+          end
+        END
+      end
+
+  end# UniversalAccessor
+
+
+end# Xot
+
+
 class Module
 
-  def universal_accessor (*names, **options)
-    names.each do |name|
-      define_universal_accessor name, options
-    end
-  end
-
-  private
-
-    def define_universal_accessor (name, options)
-      set = "ua_set_#{name}__"
-      get = "ua_get_#{name}__"
-      class_eval <<-END
-        alias_method :#{set}, options[:writer] || :#{name}=
-        alias_method :#{get}, options[:reader] || :#{name}
-        private :#{set}, :#{get}
-        def #{name} (*args)
-          #{set} *args unless args.empty?
-          #{get}
-        end
-      END
-    end
+  include Xot::UniversalAccessor
 
 end# Module
