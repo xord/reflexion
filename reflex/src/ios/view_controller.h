150f56aeb4fb66543787eb002f624c78920c6db2
xordog <xordog@gmail.com>
2019-06-05 01:02:56 +0900

    fix compile error.

--- reflex/src/ios/view_controller.h
+++ reflex/src/ios/view_controller.h
@@ -10,3 +10,5 @@
 typedef ReflexViewController* (*ReflexViewController_CreateFun) ();
 
 void ReflexViewController_set_create_fun (ReflexViewController_CreateFun fun);
+
+ReflexViewController* ReflexViewController_create ();
