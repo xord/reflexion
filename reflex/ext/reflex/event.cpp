14a024c460a71d56acea9b9d6ab5c162ffc86160
xordog <xordog@gmail.com>
2021-10-27 02:39:56 +0900

    raise error on creating Event object

--- reflex/ext/reflex/event.cpp
+++ reflex/ext/reflex/event.cpp
@@ -1,6 +1,7 @@
 #include "reflex/ruby/event.h"
 
 
+#include "reflex/exception.h"
 #include "defs.h"
 
 
@@ -10,6 +11,13 @@ RUCY_DEFINE_VALUE_FROM_TO(Reflex::Event)
 
 #define CHECK RUCY_CHECK_OBJ(Reflex::Event, self)
 
+
+static
+RUCY_DEF_ALLOC(alloc, klass)
+{
+	Reflex::reflex_error(__FILE__, __LINE__, "can not instantiate Event class.");
+}
+RUCY_END
 
 static
 RUCY_DEF0(block)
@@ -44,6 +52,7 @@ Init_event ()
 	Module mReflex = define_module("Reflex");
 
 	cEvent = mReflex.define_class("Event");
+	cEvent.define_alloc_func(alloc);
 	cEvent.define_method("block",    block);
 	cEvent.define_method("blocked?", is_blocked);
 	cEvent.define_method("time",     get_time);
